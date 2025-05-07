0878ecb3fe48c04189379f535e2c814d6905993c
Nick <bogdanovichkolia1990@gmail.com>
2025-05-07 19:42:55 +0300

    done reviews section

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -8,3 +8,20 @@ export function mobileMenuHandler() {
 export function modalWindowHandler() {
 	DOM.modalWindow.classList.toggle('is-open');
 }
+
+export function iframeHandler(event) {
+	if (event.matches) {
+		const title = document.querySelector('#reviews .section-main-title');
+		const slides = document.querySelectorAll('.swiper-slide');
+		slides.forEach(slide => {
+			const newTitle = title.cloneNode(true);
+			slide.prepend(newTitle);
+		});
+		title.remove();
+		return;
+	}
+	const newTitles = document.querySelectorAll('#reviews .section-main-title');
+	const title = newTitles[0].cloneNode(true);
+	document.querySelector('#reviews .container').prepend(title);
+	newTitles.forEach(titleForRemove => titleForRemove.remove());
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import { DOM } from './js/navigation';
-import { mobileMenuHandler, modalWindowHandler } from './js/handlers';
+import * as HANDLERS from './js/handlers';
 
-DOM.mobileMenuToggleButtons.forEach(button => button.addEventListener('click', mobileMenuHandler));
-DOM.toggleModalButtons.forEach(button => button.addEventListener('click', modalWindowHandler));
-DOM.navigationLinks.forEach(link => link.addEventListener('click', mobileMenuHandler));
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+const mediaQuery = window.matchMedia('(min-width: 1440px)');
+const swiper = new Swiper('.swiper', {
+	modules: [Pagination],
+	allowTouchMove: false,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		renderBullet: function (index, className) {
+			return `<span class="${className}">0${index + 1}</span>`;
+		},
+	},
+});
+
+DOM.mobileMenuToggleButtons.forEach(button => button.addEventListener('click', HANDLERS.mobileMenuHandler));
+DOM.toggleModalButtons.forEach(button => button.addEventListener('click', HANDLERS.modalWindowHandler));
+DOM.navigationLinks.forEach(link => link.addEventListener('click', HANDLERS.mobileMenuHandler));
+mediaQuery.addEventListener('change', HANDLERS.iframeHandler);
+
+iframeHandler(mediaQuery);
