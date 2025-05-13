4192530b915204dc84c95b5a53cdcdc83dd7d49a
Nick <bogdanovichkolia1990@gmail.com>
2025-05-13 18:57:28 +0300

    done available modal in order

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,13 +1,22 @@
 import { DOM } from './navigation';
-import whiteMob from '../img/order/order-white-mob.jpg';
 
 export function mobileMenuHandler() {
 	DOM.mobileMenu.classList.toggle('is-open');
 	DOM.mobileMenuToggleButtons.forEach(button => button.classList.toggle('hidden'));
 }
 
-export function modalWindowHandler() {
-	DOM.modalWindow.classList.toggle('is-open');
+export function modalWindowHandler(event) {
+	if (event.currentTarget.closest('[data-inner-toggle]')) {
+		DOM.modalAvailableWindow.classList.toggle('is-open');
+		DOM.modalWindow.classList.toggle('is-open');
+		return;
+	}
+	if (event.currentTarget.hasAttribute('data-modal-available')) {
+		DOM.modalAvailableWindow.classList.toggle('is-open');
+	} else if (event.currentTarget.hasAttribute('data-modal')) {
+		DOM.modalWindow.classList.toggle('is-open');
+	}
+	document.body.classList.toggle('no-scroll');
 }
 
 export function iframeHandler(event) {
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,9 +1,10 @@
 export const DOM = {
 	mobileMenuToggleButtons: document.querySelectorAll('[data-mobile]'),
 	mobileMenu: document.querySelector('.mobile-menu'),
-	toggleModalButtons: document.querySelectorAll('[data-modal]'),
+	toggleModalButtons: document.querySelectorAll('[data-toggle-modal]'),
 	modalWindow: document.querySelector('.backdrop'),
 	navigationLinks: document.querySelectorAll('.mobile-menu a'),
 	radioButtons: document.querySelectorAll('.order-radio'),
 	changebleImage: document.querySelectorAll('.order-picture'),
+	modalAvailableWindow: document.querySelector('#modal-available'),
 };
